100042cd704ddf11eb1b02c3e65a1cee989c3f6f
ruljaka <ruljaka@gmail.com>
2022-04-06 12:31:07 +0300

    Lesson 2 : 2.1 'Homework'

--- app/src/main/java/com/ruslangrigoriev/helloconstraint/MainActivity.kt
+++ app/src/main/java/com/ruslangrigoriev/helloconstraint/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.ruslangrigoriev.helloconstraint
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import helloconstraint.R
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_MESSAGE = "com.ruslangrigoriev.helloconstraint.extra.MESSAGE"
+        const val EXTRA_COUNT = "com.ruslangrigoriev.helloconstraint.extra.COUNT"
+    }
 
-    private val LOG_TAG = MainActivity::class.java.simpleName
     private var mCount = 0
     private var mShowCount: TextView? = null
     private var mZeroBtn: Button? = null
@@ -23,17 +26,19 @@ class MainActivity : AppCompatActivity() {
         mZeroBtn = findViewById(R.id.button_zero)
     }
 
-    fun showToast(view: View) {
-        val toast: Toast = Toast.makeText(
-            this, R.string.toast_message,
-            Toast.LENGTH_SHORT
-        )
-        toast.show()
+    fun sayHello(view: View) {
+        val intent = Intent(this, HelloActivity::class.java)
+        intent.putExtra(EXTRA_MESSAGE, mCount.toString())
+        startActivity(intent)
     }
 
     fun countUp(view: View) {
         mCount++
         mShowCount?.text = mCount.toString()
+        setButtonsColor(view)
+    }
+
+    private fun setButtonsColor(view: View) {
         if (mCount % 2 == 0) {
             view.setBackgroundColor(getColor(R.color.blue))
         } else {
@@ -49,4 +54,17 @@ class MainActivity : AppCompatActivity() {
         val mCountBtn = findViewById<Button>(R.id.button_count)
         mCountBtn?.setBackgroundColor(getColor(R.color.colorPrimary))
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        mCount = savedInstanceState.getInt(EXTRA_COUNT)
+        mShowCount?.text = mCount.toString()
+        setButtonsColor(findViewById(R.id.button_count))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(EXTRA_COUNT,mCount)
+    }
+
 }
